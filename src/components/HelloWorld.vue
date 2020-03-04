6d8af1b3a1e9873269fffbbc9d957f29bd26aa55
suist <skoco73@gmail.com>
2020-03-05 00:36:40 +0900

    new

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,6 +28,9 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+
+
+      <div>Hello welcome to my site</div>
     </ul>
   </div>
 </template>
