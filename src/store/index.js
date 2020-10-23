d2519c45f443f9f8b7cd9232c80d2e965898c1d3
su <skoco73@gmail.com>
2020-10-23 18:27:27 +0900

    수정

--- src/store/index.js
+++ src/store/index.js
@@ -1,53 +1,47 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     idToken: null,
-    userId:null
+    userId: null,
   },
   mutations: {
-    authUser (state, userData) {
-      state.idToken = userData.token
-      state.userId = userData.userId
-
-    }
+    authUser(state, userData) {
+      state.idToken = userData.token;
+      state.userId = userData.userId;
+    },
   },
   actions: {
-    signup({commit}, authData){
-      axios.post('/ffyokrn',{
-        email: authData.email,
-        password:authData.password,
-        returnSecureToken : true
-      })
-      .then(res => 
-        console.log(res)
-        commit('authUser',))
-      .catch(error => console.log(error))
-    },
-    login({commit}, authData){
-      axios.post('dfsdf',{
-        email: authData.email,
-        password: authData.passeotf,
-        returnSecureToken:true
-      })
-
-    module.exports.hell = ( event, context, callback = {
-      const response = {
-        statusCode : 200,
-        body : JSON.stringify({
-          message : 'Go Serverless V1.0 !  
-          YOur function executed successfully successfully!'
-        })
-      }
-    })
-      // you can access
-      // this.$store.dispatch('signup',{email})
-
-    }
+    // signup({commit}, authData){
+    //   axios.post('/ffyokrn',{
+    //     email: authData.email,
+    //     password:authData.password,
+    //     returnSecureToken : true
+    //   })
+    //   .then(res =>
+    //     commit('authUser',)).catch(error => console.log(error))
+    // },
+    // login({commit}, authData){
+    //   axios.post('dfsdf',{
+    //     email: authData.email,
+    //     password: authData.passeotf,
+    //     returnSecureToken:true
+    //   })
+    // module.exports.hell = ( event, context, callback = {
+    //   const response = {
+    //     statusCode : 200,
+    //     body : JSON.stringify({
+    //       message : 'Go Serverless V1.0 !
+    //       YOur function executed successfully successfully!'
+    //     })
+    //   }
+    // })
+    //   // you can access
+    //   // this.$store.dispatch('signup',{email})
+    // }
   },
-  modules: {
-  }
-})
+  modules: {},
+});
